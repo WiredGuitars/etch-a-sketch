bcef7c25e70c8c601f98902e336135404ec343f3
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2023-03-07 02:18:29 -0600

    working 'clear' button

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-function addDivToContainer() {
+let button = document.querySelector("button");
+
+function initializeGrid() {
   let container = document.getElementById("container");
   for (i = 0; i < 256; i++) {
     let newDiv = document.createElement("div");
+    newDiv.classList.add("toggle-off");
     newDiv.addEventListener("mouseover", function () {
-      this.style.backgroundColor = "black";
+      newDiv.classList.remove("toggle-off");
+      newDiv.classList.add("toggle-on");
     });
     container.appendChild(newDiv);
   }
 }
-addDivToContainer();
 
+initializeGrid();
 
-let button = document.querySelector("button")
-
-button.addEventListener("click", function(){
-    let gridSize = parseInt(prompt("Grid Size?"))
-    if (gridSize > 100 || gridSize < 1){
-        
-    }
-    else console.log("hooray")
+button.addEventListener("click", function () {
+  let toggledDivs = document.querySelectorAll("div.toggle-on");
+  for (let i = 0; i < toggledDivs.length; i++) {
+    toggledDivs[i].classList.remove("toggle-on");
+    toggledDivs[i].classList.add("toggle-off");
+  }
 })
-     
